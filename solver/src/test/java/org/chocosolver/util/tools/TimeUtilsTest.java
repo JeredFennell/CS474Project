ee58e0173f1ff6ee79f11591dff07a2e6b138391
Madi Thompson <madithompson@u.boisestate.edu>
2025-04-19 23:37:29 -0600

    edit tests

--- solver/src/test/java/org/chocosolver/util/tools/TimeUtilsTest.java
+++ solver/src/test/java/org/chocosolver/util/tools/TimeUtilsTest.java
@@ -6,28 +6,28 @@ import org.testng.annotations.Test;
 public class TimeUtilsTest {
 
     @Test
-    public void convertToSeconds1() {
+    public void convertToSecondsBasic() {
         long expected = 9000;
         long result = TimeUtils.convertInSeconds("2h30m");
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void convertToSeconds2() {
+    public void convertToSecondsHalfSecond() {
         long expected = 11;
         long result = TimeUtils.convertInSeconds("11.5s");
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void convertToSeconds3() {
+    public void convertToSeconds59m59s() {
         long expected = 100799;
         long result = TimeUtils.convertInSeconds("1d3h59m59s");
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void convertToSeconds4() {
+    public void convertToSeconds0Duration() {
         // it accepts 0 as an input, but NOT 0s or 0m on their own -- is that how it should work?
         // it also accepts 0m30s
         long expected = 0;
@@ -36,9 +36,22 @@ public class TimeUtilsTest {
     }
 
     @Test
-    public void convertToSeconds5() {
+    public void convertToSeconds0m30s() {
         long expected = 30;
         long result = TimeUtils.convertInSeconds("0m30s");
         Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void convertToSecondsNeg() {
+        long expected = 90;
+        long result = TimeUtils.convertInSeconds("-1m30s");
+        Assert.assertEquals(result, expected);
+    }
+
+    // null duration is not handled in code
+    @Test
+    public void convertToSecondsNull() {
+        long result = TimeUtils.convertInSeconds(null);
     }
 }
