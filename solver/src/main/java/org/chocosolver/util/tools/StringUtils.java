61e32b975aa87a2db3b927eb6d25f3740ef20b47
Madi Thompson <madithompson@u.boisestate.edu>
2025-04-19 23:16:31 -0600

    edit comments

--- solver/src/main/java/org/chocosolver/util/tools/StringUtils.java
+++ solver/src/main/java/org/chocosolver/util/tools/StringUtils.java
@@ -45,10 +45,10 @@ public class StringUtils {
      * @return padded string
      */
     /*  --- Properties ---
-     *  input str - cannot be null and can be any length string.
-     *  input padlen - can be any int, positive or negative or 0.
-     *  input pad - cannot be null and can be any length string.
-     *  return string - length of return string == (padlen * pad.length()) OR str.length() (whichever is bigger).
+     *  input str:     not nullable, any length string.
+     *  input padlen:  any int, positive or negative or 0.
+     *  input pad:     not nullable, any length string.
+     *  return string: length of string == (padlen * pad.length()) OR str.length() (whichever is bigger).
      */
     public static String pad(String str, int padlen, String pad) {
         // --- Input assertions ---
