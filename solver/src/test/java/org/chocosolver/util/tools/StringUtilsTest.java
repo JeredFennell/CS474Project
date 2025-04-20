3c761f1e4074e243aa857602f5fa3934ac18001c
Madi Thompson <madithompson@u.boisestate.edu>
2025-04-19 22:39:39 -0600

    add more tests and add assertions

--- solver/src/test/java/org/chocosolver/util/tools/StringUtilsTest.java
+++ solver/src/test/java/org/chocosolver/util/tools/StringUtilsTest.java
@@ -21,10 +21,58 @@ public class StringUtilsTest {
     }
 
     @Test
-    public void testPadEmpty() {
+    public void testPad1len() {
         String expected = "M";
         String result = StringUtils.pad("M", 1, "#");
         Assert.assertEquals(result, expected);
     }
 
+    @Test
+    public void testPad0len() {
+        String expected = "M";
+        String result = StringUtils.pad("M", 0, "#");
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void testPad0lenEmpty() {
+        String expected = "";
+        String result = StringUtils.pad("", 0, "#");
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void testPadLongStr() {
+        String expected = "Mxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+        String result = StringUtils.pad("Mxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                60, "#");
+        Assert.assertEquals(result, expected);
+    }
+
+//    @Test
+//    public void testPadNullStr() {
+//        String result = StringUtils.pad(null, 10, "#");
+//    }
+
+    // TODO I think this is a bug - you can add 'null' as padding. null should not be accepted.
+    @Test
+    public void testPadNullPad() {
+        String expected = "M";
+        String result = StringUtils.pad("M", -10, null);
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void testPadNullPad2() {
+        String expected = "M";
+        String result = StringUtils.pad("M",2 , null);
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void testPadEmpty() {
+        String expected = "M";
+        String result = StringUtils.pad("M", -10, "");
+        Assert.assertEquals(result, expected);
+    }
 }
