8a73a1c636c128a6dae490ea77d126074ef66c0a
Madi Thompson <madithompson@u.boisestate.edu>
2025-04-21 15:00:04 -0600

    update tests

--- solver/src/test/java/org/chocosolver/util/tools/TimeUtilsTest.java
+++ solver/src/test/java/org/chocosolver/util/tools/TimeUtilsTest.java
@@ -6,28 +6,28 @@ import org.testng.annotations.Test;
 public class TimeUtilsTest {
 
     @Test
-    public void convertToSecondsBasic() {
+    public void convert_basic() {
         long expected = 9000;
         long result = TimeUtils.convertInSeconds("2h30m");
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void convertToSecondsHalfSecond() {
+    public void convert_half_second() {
         long expected = 11;
         long result = TimeUtils.convertInSeconds("11.5s");
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void convertToSeconds59m59s() {
+    public void convert_59m59s() {
         long expected = 100799;
         long result = TimeUtils.convertInSeconds("1d3h59m59s");
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void convertToSeconds0Duration() {
+    public void convert_0_duration() {
         // it accepts 0 as an input, but NOT 0s or 0m on their own -- is that how it should work?
         // it also accepts 0m30s
         long expected = 0;
@@ -36,22 +36,36 @@ public class TimeUtilsTest {
     }
 
     @Test
-    public void convertToSeconds0m30s() {
+    public void convert_0m30s() {
         long expected = 30;
         long result = TimeUtils.convertInSeconds("0m30s");
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void convertToSecondsNeg() {
+    public void convert_neg() {
         long expected = 90;
         long result = TimeUtils.convertInSeconds("-1m30s");
         Assert.assertEquals(result, expected);
     }
 
-    // null duration is not handled in code
     @Test
-    public void convertToSecondsNull() {
-        long result = TimeUtils.convertInSeconds(null);
+    public void convert_syntax_error() {
+        long expected = 0;
+        long result = TimeUtils.convertInSeconds("20");
+        Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void convert_out_of_order() {
+        long expected = 200;
+        long result = TimeUtils.convertInSeconds("20s3m0h");
+        Assert.assertEquals(result, expected);
+    }
+
+    // null duration is not handled in code
+//    @Test
+//    public void convertToSecondsNull() {
+//        long result = TimeUtils.convertInSeconds(null);
+//    }
 }
